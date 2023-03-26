2eacbf2cdc2bcc458e5d4e937179b123368dbb8d
Roy Ben Haroosh <roybh@cisco.com>
2023-03-26 18:22:41 +0300

    minor suffix edit

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require('core')
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('FormatOnSave', { clear = true }),
-  pattern = { '*.go', '*.lua' },
+  pattern = { '*.go', '*.lua', '.ts', '.tsx' },
   callback = function(opts)
     require('keymap.utils').format_buffer(opts.buf)
   end,
